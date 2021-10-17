7bed11a5ee21abf1ad2aa31ca1a5906678b1413f
Friedsoda <friedsoda1992@gmail.com>
2021-10-17 15:55:54 +0100

    initial

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/manmantou/Desktop/Study/TCD/ComputerGraphics/Lab1
-# Build directory: /Users/manmantou/Desktop/Study/TCD/ComputerGraphics/Lab1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
