7ca1b75e3c1614e8492ba6e026be68ede0cbf996
Friedsoda <friedsoda1992@gmail.com>
2021-10-31 22:16:06 +0000

    add model

--- src/Shaders/character.frag
+++ src/Shaders/character.frag
@@ -1,10 +1,44 @@
 #version 330 core
 
-in vec2 TexCoords;
+struct DirLight {
+    vec3 direction;
+	
+    float ambient;
+    float diffuse;
+    vec3 specular;
+};
+
 out vec4 FragColor;
+
+in vec3 FragPos;
+in vec3 Normal;
+in vec2 TexCoords;
+
 uniform sampler2D texture_diffuse1;
+uniform vec3 viewPos;
+uniform DirLight dirLight;
+
+vec4 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir);
 
 void main()
 {    
-    FragColor = texture(texture_diffuse1, TexCoords);
+    vec3 norm = normalize(Normal);
+    vec3 viewDir = normalize(viewPos - FragPos);
+
+    FragColor = CalcDirLight(dirLight, norm, viewDir);
+}
+
+vec4 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir)
+{
+    vec3 lightDir = normalize(-light.direction);
+    // diffuse 
+    float diff = max(dot(normal, lightDir), 0.0);
+    // specular 
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    
+    vec4 ambient = light.ambient * texture(texture_diffuse1, TexCoords);
+    vec4 diffuse = light.diffuse * diff * texture(texture_diffuse1, TexCoords);
+    vec3 specular = light.specular * spec;
+    return (ambient + diffuse + vec4(specular, 1.0));
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include "glheader.h"
+#include "Model.h"
+#include <filesystem>
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -6,51 +8,6 @@ void processInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 1200;
 const unsigned int SCR_HEIGHT = 800;
 
-// MARK: Model Data
-float vertices[] = {
-    // positions          // normals           // texture coords
-    -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
-     0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  0.0f,
-     0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
-     0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f,  1.0f,
-    -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  1.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f,  0.0f,
-
-    -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
-     0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  0.0f,
-     0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
-     0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f,  1.0f,
-    -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  1.0f,
-    -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f,  0.0f,
-
-    -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
-    -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
-    -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
-    -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
-    -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
-    -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
-
-     0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
-     0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  1.0f,
-     0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
-     0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  1.0f,
-     0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f,  0.0f,
-     0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  1.0f,  0.0f,
-
-    -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
-     0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  1.0f,
-     0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
-     0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  1.0f,  0.0f,
-    -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  0.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,  0.0f,  1.0f,
-
-    -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f,
-     0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  1.0f,
-     0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
-     0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f,  0.0f,
-    -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  0.0f,
-    -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f,  1.0f
-};
 
 // Camera
 Camera camera(glm::vec3(0, 0, 3.0f), glm::radians(15.0f), glm::radians(180.0f), glm::vec3(0, 1.0f, 0));
@@ -62,6 +19,9 @@ glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 float lastX;
 float lastY;
 bool firstMouse = true;
+
+// Move character
+float step = 0.0f;
 
 void processInput(GLFWwindow *window)
 {
@@ -80,11 +40,11 @@ void processInput(GLFWwindow *window)
     }
     else if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
     {
-        camera.speedX = -1.0f;
+        step = step - 0.1f;
     }
     else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
     {
-        camera.speedX = 1.0f;
+        step = step + 0.1f;
     }
     else if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
     {
@@ -151,40 +111,15 @@ int main(int argc, const char *argv[])
     // Open deep test
     glEnable(GL_DEPTH_TEST);
 
-    // Init Shader
-    Shader* triangleShader1 = new Shader("../src/Shaders/vertexShaderSource.vert", "../src/Shaders/fragmentShaderSource1.frag");
+    stbi_set_flip_vertically_on_load(true);
+
+    // Init shader
+    Shader myShader("../src/Shaders/character.vert", "../src/Shaders/character.frag");
     // Shader* triangleShader2 = new Shader("../src/Shaders/vertexShaderSource.vert", "../src/Shaders/fragmentShaderSource2.frag");
 
-    // VAO & VBO
-    unsigned int VAOs[2];
-    glGenVertexArrays(2, VAOs);  
+    // Load model
+    Model character(filesystem::path("/Users/manmantou/Desktop/Study/TCD/ComputerGraphics/Lab1/src/Models/donut/Donut.obj"));
 
-    unsigned int VBOs[2];
-    glGenBuffers(2, VBOs);
-
-    glBindVertexArray(VAOs[0]);
-    glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
-
-    // glBindVertexArray(VAOs[1]);
-    // glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
-    // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2) + sizeof(colors), NULL, GL_STATIC_DRAW);
-    // glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices2), vertices2);
-    // glBufferSubData(GL_ARRAY_BUFFER, sizeof(vertices2), sizeof(colors), colors);
-
-    // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    // glEnableVertexAttribArray(0);
-    // glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(9 * sizeof(float)));
-    // glEnableVertexAttribArray(1);
-
-    // Prepare MVP Matrices
     glm::mat4 modelMat(1.0f);
     glm::mat4 viewMat(1.0f);
     glm::mat4 projMat(1.0f);
@@ -204,30 +139,23 @@ int main(int argc, const char *argv[])
         viewMat = camera.GetViewMatrix();
 
         // MVP
-        modelMat = glm::translate(glm::mat4(1.0f), glm::vec3( 0.0f,  0.0f,  0.0f));
-        triangleShader1->use();
-        triangleShader1->setVec3("viewPos", camera.Position);
-        triangleShader1->setMat4("modelMat", modelMat);
-        triangleShader1->setMat4("viewMat", viewMat);
-        triangleShader1->setMat4("projMat", projMat);
+        modelMat = glm::translate(glm::mat4(1.0f), glm::vec3(step,  0.0f,  0.0f));
+        modelMat = glm::scale(modelMat, glm::vec3(0.3f, 0.3f, 0.3f));	// it's a bit too big for our scene, so scale it down
+        myShader.use();
+        myShader.setVec3("viewPos", camera.Position);
+        myShader.setMat4("modelMat", modelMat);
+        myShader.setMat4("viewMat", viewMat);
+        myShader.setMat4("projMat", projMat);
 
+        // Lighting
         // Directional light
-        triangleShader1->setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
-        triangleShader1->setVec3("dirLight.ambient", 0.5f, 0.3f, 0.2f);
-        triangleShader1->setVec3("dirLight.diffuse", 0.4f, 0.4f, 0.4f);
-        triangleShader1->setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
+        myShader.setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
+        myShader.setFloat("dirLight.ambient", 0.5f);
+        myShader.setFloat("dirLight.diffuse", 0.4f);
+        myShader.setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
 
-        // Material
-        triangleShader1->setFloat("material.shininess", 32.0f);
-        triangleShader1->setFloat("material.diffuse", 0.5);
-        triangleShader1->setFloat("material.specular", 1.0);
-
-        glBindVertexArray(VAOs[0]);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-
-        // triangleShader2->use();
-        // glBindVertexArray(VAOs[1]);
-        // glDrawArrays(GL_TRIANGLES, 0, 3);
+        // Model
+        character.Draw(myShader);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
